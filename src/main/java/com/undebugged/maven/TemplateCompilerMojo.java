f9b0a2bb23c864caacfb28abed20815b020ee3d6
Guido Grazioli <guido.grazioli@gmail.com>
2013-10-06 15:20:30 +1100

    port stuff from my fork of nominum plugin

--- src/main/java/com/undebugged/maven/TemplateCompilerMojo.java
+++ src/main/java/com/undebugged/maven/TemplateCompilerMojo.java
@@ -18,7 +18,11 @@ package com.undebugged.maven;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+
 import play.templates.TemplateCompilationError;
 import scala.collection.JavaConversions;
 
